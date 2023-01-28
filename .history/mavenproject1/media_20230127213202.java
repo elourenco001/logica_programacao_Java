e45dcf496b52b100a31e42d470e2138da2af8697
devlourenco <oliverlourenco001@gmail.com>
2023-01-27 21:33:29 -0300

    Programa de Média

---
+++ .history/mavenproject1/media_20230127213202.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main( String [] args){
+        Scanner sc = new Scanner (System.in);
+        double x,y,media;
+
+        System.out.println("Digite o primeiro numero: ");
+        x = sc.nextDouble;
+        System.out.println("Digite o segundo numero: ");
+        y = sc.nextDouble;
+        media = (x+y)/2.0;
+        System.out.println("O Valor da média é: " + media);
+        sc.close()
+    }
+
+}
+    
