6bd9557ab8f2aaaebef945e14688e25677ecac38
Emerson Lourenco <emersonlourenco@iMac-de-Emerson.local>
2023-02-21 19:44:08 -0300

    teste_matriz

---
+++ C/Matriz/teste_matriz.c
@@ -0,0 +1,34 @@
+//Fazer um programa para ler dois números inteiros positivos M e N(máximo = 5), depois ler uma matriz de M linhas e N colunas
+//contendo números inteiros. Em seguida, mostrar na tela a matriz lida
+
+#include <stdio.h>
+
+int main(){
+
+    int L, C, i, j;
+    int mat[4][4];
+
+    printf("Quantas linhas vai ter a Matriz? ");
+    scanf("%d", &L);
+    printf("Quantas colunas vai ter a Matriz? ");
+    scanf("%d", &C);
+
+    for (i = 0; i < L; i++){
+        for (j = 0; j < C; j++){
+            printf("Elemento [%d , %d]", i, j);
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    printf("\n");
+    
+    for(i = 0; i < L; i++){
+        for(j = 0; j < C; j++){
+            printf("%d ",mat[i][j]);
+
+        }
+    printf("\n");
+
+    }
+
+    return 0;
+}
